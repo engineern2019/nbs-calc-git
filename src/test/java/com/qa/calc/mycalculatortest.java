54f219219772b491dd6cad8d0b219cb5c6d9d05a
engineern2019 <alidawood15@gmail.com>
2019-06-24 16:19:06 +0100

    test added to each feature

--- src/test/java/com/qa/calc/mycalculatortest.java
+++ src/test/java/com/qa/calc/mycalculatortest.java
@@ -11,16 +11,19 @@ public class mycalculatortest {
 		int result = calculator.add(2,2);
 		Assert.assertEquals(4,  result);
 	}
+	@Test
 	public void subtractTwoNumbersTest() {
 		calculator calculator = new calculator();
 		int result = calculator.subtract(10,5);
 		Assert.assertEquals(5, result);
 	}
+	@Test
 	public void multiplyTwoNumberstest() {
 		calculator calculator = new calculator();
 		int result = calculator.multiply(2,10);
 		Assert.assertEquals(20, result);
 	}
+	@Test
 	public void divideTwoNumberstest() {
 		calculator calculator = new calculator();
 		int result = calculator.divide(10,2);
